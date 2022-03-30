b54adccc2c4d7f480661dca2f5347387c1f8b1df
JoseGonzalez <70743629+JoseGonzalez-git@users.noreply.github.com>
2022-03-29 22:00:01 -0500

    first commit

---
+++ ExamenPracticoClasificacion/solucion.py
@@ -0,0 +1,31 @@
+#NO ELIMINAR LAS SIGUIENTES IMPORTACIONES, sirven para probar tu código en consola, y el funcionamiento de la librería csv respectivamente
+from test import tester
+import csv
+
+"""NOTAS: 
+    - PARA ESTE RETO PUEDES PROBAR TU PROGRAMA, DANDO CLICK EN LA NAVE ESPACIAL
+    - LA CONSOLA TE DIRÁ SI TU SOLUCIÓN ES CORRECTA O NO
+    - NO olvidar evaluar tu solución
+"""
+
+
+"""Inicio espacio para programar funciones propias"""
+#En este espacio podrás programar las funciones que deseas usar en la función solución (ES OPCIONAL)
+
+
+
+"""Fin espacio para programar funciones propias"""
+
+def solucion():
+    #ESTA ES LA FUNCIÓN A LA QUE LE DEBES GARANTIZAR LOS RETORNOS REQUERIDOS EN EL ENUNCIADO.
+    
+    
+    
+    return fecha_menor_precio, menor_precio, fecha_mayor_precio, mayor_precio, variacion_diaria_media
+
+"""
+NO COLOCAR CÓDIGO FUERA DE LAS FUNCIONES QUE USTED DESARROLLE
+Esta línea de código que sigue permite saber si su solución al ejercicio es correcto
+Por favor NO ELIMINARLA, NO MODIFICARLA
+"""
+tester(solucion)
