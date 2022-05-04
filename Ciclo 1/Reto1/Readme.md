16fa2ff8247349e712d5d0fe10e2c1fb2d8ea225
Jose Gonzalez <JoseGonzalez-git>
2022-05-04 10:11:04 -0500

    Reto 1- Ciclo 1

---
+++ Ciclo 1/Reto1/Readme.md
@@ -0,0 +1,10 @@
+# Reto 1 - Ciclo 1
+
+Para saber más sobre el reto, visita la [el pdf del reto](Reto%201.pdf).
+
+La solucion planteada reside en los siguientes temas:
+
+- `Ciclo While`: Ya que no se establece un limite de iteraciones(no habia limite en los intentos), se puede utilizar un ciclo `while` para realizar la iteracion definiendo un "validador" siempre con valor "True" y reescibiendolo como "False" para finalzar el mismo cuando la condicion de "n==b" se cumpliera.
+- `Condicionales`: Para realizar la comparaciones establecidas en el enunciado.Ej: Si el numero dado por el usuario "n" era mayor a numero predefinido por el sistema ha adivinar "b".
+
+[Calificacion_del_Reto](calificacion.png) > Máxima : 5 de 5.
---
+++ Ciclo 1/Reto1/solution.py
@@ -0,0 +1,34 @@
+"""
+NO ELIMINAR LAS SIGUIENTES IMPORTACIONES, 
+El equipo de desarrollo de este calificador modificó las funciones 'print' e 'input'.
+Esta modificación se hizo con la finalidad de que el sistema pueda calificar tu solución.
+Por eso LEER MUY BIEN LO QUE SE SOLICITA Y LAS RESTRICCIONES QUE SE LE IMPUSIERON A ESTAS DOS FUNCIONES.
+"""
+# from student_utilities import input, print
+
+
+def solucion(b, n):
+    # ACÁ INICIA LA FUNCIÓN SOLUCIÓN (En este espacio debes entregar tu solución)
+    validador = True
+    𝑐𝑎𝑛𝑡𝑖𝑑𝑎𝑑_𝑖𝑛𝑡𝑒𝑛𝑡𝑜s = 0
+    while validador:
+        valor_ingresado = int(input(f"Ingrese un numero entre {0} y {b}: "))
+        if valor_ingresado < 0 or valor_ingresado > b:
+            print("¡Te saliste del intervalo!")
+        elif valor_ingresado > n:
+            𝑐𝑎𝑛𝑡𝑖𝑑𝑎𝑑_𝑖𝑛𝑡𝑒𝑛𝑡𝑜s += 1
+            print("¡Ups! Te pasaste")
+        elif valor_ingresado < n:
+            𝑐𝑎𝑛𝑡𝑖𝑑𝑎𝑑_𝑖𝑛𝑡𝑒𝑛𝑡𝑜s += 1
+            print("¡Ups! Estás por debajo")
+        elif valor_ingresado == n:
+            𝑐𝑎𝑛𝑡𝑖𝑑𝑎𝑑_𝑖𝑛𝑡𝑒𝑛𝑡𝑜s += 1
+            print(f"¡LO LOGRASTE! Usaste {𝑐𝑎𝑛𝑡𝑖𝑑𝑎𝑑_𝑖𝑛𝑡𝑒𝑛𝑡𝑜s} intentos")
+            validador = False
+
+    # ACÁ TERMINA LA FUNCIÓN SOLUCIÓN
+"""
+¡IMPORTANTE! ¡IMPORTANTE! ¡IMPORTANTE! ¡IMPORTANTE! ¡IMPORTANTE! ¡IMPORTANTE! ¡IMPORTANTE! ¡IMPORTANTE!
+NO AÑADIR CÓDIGO FUERA DE LA FUNCIÓN calcular_promedio_y_cuadro_honor(grupo) .
+SOLO AÑADIR CÓDIGO ENTRE EL ESPACIO DONDE DICE: ACÁ INICIA... ACÁ TERMINA
+"""
