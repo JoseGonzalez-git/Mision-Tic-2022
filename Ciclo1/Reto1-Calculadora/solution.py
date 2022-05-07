fd40302c89ecd6f25b059195fdeff2266aa0d86f
Jose Gonzalez <70743629+JoseGonzalez-git@users.noreply.github.com>
2022-05-07 17:28:46 -0500

    completed

--- Ciclo1/Reto1-Calculadora/solution.py
+++ Ciclo1/Reto1-Calculadora/solution.py
@@ -7,32 +7,43 @@ Por eso LEER MUY BIEN LO QUE SE SOLICITA Y LAS RESTRICCIONES QUE SE LE IMPUSIERO
 # from student_utilities import input, print
 
 
+def detener(subtotal):
+    continuar_compra = input("¿Faltan productos por cobrar S o N?: ")
+    if continuar_compra.upper() == 'N':
+        print(f'TOTAL A COBRAR: {subtotal}')
+        return False
+    elif continuar_compra.upper() == 'S':
+        return True
+    else:
+        continuar_compra = input("¿Faltan productos por cobrar S o N?: ")
+
+
 def solucion():
-    #ACÁ INICIA LA FUNCIÓN SOLUCIÓN (En este espacio debes entregar tu solución)
-    validador =True
+    # ACÁ INICIA LA FUNCIÓN SOLUCIÓN (En este espacio debes entregar tu solución)
+    validador = True
+    precio_unitario = 0
+    subtotal = 0
     while(validador):
         precio_unitario = int(input("Ingrese el valor unitario del producto: "))
-        validar_iva = input("¿El productio tiene IVA S\N?: ")
-        if (validar_iva.upper == 'S'):
+        validar_iva = input("¿El productio tiene IVA S o N?: ")
+        if (validar_iva.upper() == 'S'):
             print("IVA incluído")
-            cantidad_producto = int(input("Cantidad a cobrar del producto"))
-            subtotal += (precio_unitario + (0.19 * precio_unitario ))*cantidad_producto 
+            cantidad_producto = int(input("Cantidad a cobrar del producto: "))
+            subtotal += (precio_unitario +
+                         (0.19 * precio_unitario))*cantidad_producto
             print(f"SUBTOTAL: {subtotal}")
-            continuar_comprar = input("¿Faltan productos por cobrar S\N?").upper
-            if continuar_comprar == 'S' :continue
-            else: 
-                validador= False
-                print(f'TOTAL A COBRAR: {subtotal}')
-        elif(validar_iva.upper == 'N'):
-            cantidad_producto = int(input("Cantidad a cobrar del producto"))
+            validador = detener(subtotal)
+        elif(validar_iva.upper() == 'N'):
+            cantidad_producto = int(input("Cantidad a cobrar del producto: "))
             print("PRODUCTO SIN IVA")
-            subtotal += precio_unitario*cantidad_producto 
+            subtotal += precio_unitario*cantidad_producto
             print(f"SUBTOTAL: {subtotal}")
-            if continuar_comprar == 'S' :continue
-            else: 
-                validador= False
-                print(f'TOTAL A COBRAR: {subtotal}')
+            validador = detener(subtotal)
+        else:
+            validar_iva = input("¿El productio tiene IVA S o N?: ")
 
-    #ACÁ TERMINA LA FUNCIÓN SOLUCIÓN
+
+    # ACÁ TERMINA LA FUNCIÓN SOLUCIÓN
+
 
 solucion()
